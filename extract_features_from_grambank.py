689ee14781519525474d1623a5d1fd12020e0ecc
Lara Aasem <aasem.lara@gmail.com>
2026-04-20 17:59:12 +0200

    feat: WIP feature extraction script logic

--- extract_features_from_grambank.py
+++ extract_features_from_grambank.py
@@ -1,17 +1,29 @@
-# Package assumes python3 and pip3 installed in environment.
-# TODO: Add comment about csv file path and grambank version.
+# Script was run on metadata file for Grambank v1.0.2 and assumes pycldf is installed in environment.
 
-# Install and import needed package to read CLDF datasets
-# pip install pycldf
-# from pycldf.dataset import Dataset
+from itertools import groupby
+from pycldf.dataset import Dataset
 
-# Initialize constant of Egyptian Arabic lang ID
+def map_language_names_to_ids(languages):
+    language_names_to_ids = {}
+    for language in languages:
+        if "Arabic" not in language["Name"] and "Coptic" not in language["Name"]: continue
 
-# Initialize constant of Coptic lang ID
+        language_names_to_ids[language["Name"]] = language["ID"]
+    return language_names_to_ids
 
-# Initialize constant array of other Arabic dialect lang IDs
+def get_positive_features(values):
+    values_to_exclude = ["0", None]
+    return [value for value in values if value["Value"] not in values_to_exclude]
 
-# Read values.csv file
+if __name__ == '__main__':
+    import sys
+
+    metadata_path = sys.argv[1]
+    grambank = Dataset.from_metadata(metadata_path)
+
+    language_names_to_ids = map_language_names_to_ids(grambank["LanguageTable"])
+    positive_features = get_positive_features(grambank["ValueTable"])
+    # groupby(positive_features, "Parameter_ID")
 
 # Group by Parameter_ID to generate a dict where Parameter_ID: [Array of Language_IDs]
 
@@ -19,4 +31,6 @@
 
 # Get parameter names from parameters.csv
 
-# Generate CSV with Feature (parameter name), Source ("Grambank #{ID}"), Other Arabic dialects (map using languages.csv) 
+# Generate CSV with Feature (parameter name), Source ("Grambank #{ID}"), Other Arabic dialects (map using languages.csv)
+
+# Include value (whether 1 or 2 or 3) in output
