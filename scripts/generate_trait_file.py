4a3dc0f3c603996f940e12b5489ae6c88b512e83
Lara Aasem <aasem.lara@gmail.com>
2026-05-17 18:21:47 +0200

    feat: Finalize trait file script

--- scripts/generate_trait_file.py
+++ scripts/generate_trait_file.py
@@ -54,18 +54,87 @@ LANGUAGES = [
 ]
 
 
-def filter_values(grambank, feature_id):
-    values = grambank["ValueTable"]
-    filtered_values = []
-    for value in values:
+def map_language_name_to_id(grambank_languages):
+    print("Mapping language names to IDs...")
+
+    language_id_by_name = {}
+
+    for language in LANGUAGES:
+        language_id_results = [
+            grambank_language["ID"]
+            for grambank_language in grambank_languages
+            if grambank_language["Name"] == language
+        ]
+        if not language_id_results:
+            continue
+
+        language_id = language_id_results[0]
+        language_id_by_name[language] = language_id
+
+    return language_id_by_name
+
+
+def filter_values(grambank_values, feature_id, language_ids):
+    print("Filtering values...")
+
+    values_by_language_id = {}
+
+    for value in grambank_values:
         if value["Parameter_ID"] != feature_id:
             continue
 
-        if value["Language_ID"]
+        if value["Language_ID"] not in language_ids:
+            continue
+
+        values_by_language_id[value["Language_ID"]] = value["Value"]
+
+    return values_by_language_id
+
+
+def format_data(filtered_values_by_language_id, language_id_by_name):
+    print("Formatting data...")
+
+    formatted_data = ""
+
+    for language in LANGUAGES:
+        value = get_value(filtered_values_by_language_id, language_id_by_name, language)
+        formatted_data += f"{language}\t{value}\n"
+
+    return formatted_data
+
+
+def get_value(filtered_values_by_language_id, language_id_by_name, language):
+    language_id = language_id_by_name.get(language)
+    if not language_id:
+        return 0
+
+    grambank_value = filtered_values_by_language_id.get(language_id)
+    if not grambank_value:
+        return 0
+
+    return grambank_value
+
+
+def write_to_file(data):
+    print("Writing to file...")
+
+    with open("data/trait.txt", "w", encoding="utf-8") as textfile:
+        textfile.write(data)
+
 
 if __name__ == "__main__":
     import sys
 
     metadata_path = sys.argv[1]
-    grambank = Dataset.from_metadata(metadata_path)
     feature_id = sys.argv[2]
+
+    grambank = Dataset.from_metadata(metadata_path)
+    language_id_by_name = map_language_name_to_id(grambank["LanguageTable"])
+    language_ids = list(language_id_by_name.values())
+    filtered_values_by_language_id = filter_values(
+        grambank["ValueTable"], feature_id, language_ids
+    )
+    data = format_data(filtered_values_by_language_id, language_id_by_name)
+    write_to_file(data)
+
+    print("All done!")
